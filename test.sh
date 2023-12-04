104dba76fff35119759a5d879ad0e87f407830af
YLiu <yliuhz@outlook.com>
2023-12-04 20:27:57 +0800

    Only arxiv

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export SERVERCHAN_API_KEY=SCT40638T9GkdvStHkdzTKoQySgg9N8x2
+export CAIYUN_TOKEN=d40ldcgbsfz8m2cmxt83
+export FEISHU_URL=https://open.larksuite.com/open-apis/bot/v2/hook/a49a52ce-871b-4ddb-a61e-2717d7d4660e
+
+python arxiv.py
+
+export 
